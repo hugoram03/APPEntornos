34fe714d8e93c218a526aca70e381ca9ac1ca890
Hugo <huramsim@gmail.com>
2023-02-07 12:23:37 +0100

    Primer commit

---
+++ aplicacionEntornos/src/aplicacionentornos/AplicacionEntornos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplicacionentornos;
+
+import vista.FPrincipal;
+
+/**
+ *
+ * @author Alumno
+ */
+public class AplicacionEntornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FPrincipal f = new FPrincipal();
+        f.setVisible(true);
+    }
+    
+}
